ed940c5fa68ab109771db3c85760a6e565992947
crystalDf <chendong333@gmail.com>
2016-04-11 22:57:46 +0800

    Chapter 08 Files

--- slash.py
+++ slash.py
@@ -9,3 +9,13 @@ for filename in my_files:
     print(os.path.join(path, filename))
 
 print(os.getcwd())
+
+print(os.path.abspath('.'))
+print(os.path.abspath('./Scripts'))
+print(os.path.isabs('.'))
+print(os.path.isabs(os.path.abspath('.')))
+
+print(os.path.basename(os.path.abspath('.')))
+print(os.path.dirname(os.path.abspath('.')))
+print(os.path.split(os.path.abspath('.')))
+print(os.path.abspath('.').split(os.path.sep))
