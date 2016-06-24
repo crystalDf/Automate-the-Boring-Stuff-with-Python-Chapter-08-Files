13b529ed92fc8cc769e1419ff772b8e47d5a8e6b
crystalDf <chendong333@gmail.com>
2016-06-24 21:52:05 +0800

    Chapter 08 Files

---
+++ shelve_mydata_test.py
@@ -0,0 +1,11 @@
+import shelve
+
+shelfFile = shelve.open('mydata')
+print(type(shelfFile))
+print(shelfFile['cats'])
+shelfFile.close()
+
+shelfFile = shelve.open('mydata')
+print(list(shelfFile.keys()))
+print(list(shelfFile.values()))
+shelfFile.close()
