681089780c9735f56c12ffbd60d1339e7df64abe
crystalDf <chendong333@gmail.com>
2016-06-25 20:46:37 +0800

    Chapter 08 Files

--- shelve_mydata_test.py
+++ shelve_mydata_test.py
@@ -1,4 +1,5 @@
 import shelve
+import pprint
 
 shelfFile = shelve.open('mydata')
 print(type(shelfFile))
@@ -9,3 +10,10 @@ shelfFile = shelve.open('mydata')
 print(list(shelfFile.keys()))
 print(list(shelfFile.values()))
 shelfFile.close()
+
+cats = [{'name': 'Zophie', 'desc': 'chubby'},
+        {'name': 'Pooka', 'desc': 'fluffy'}]
+pprint.pformat(cats)
+fileObj = open('myCats.py', 'w')
+print(fileObj.write('cats = ' + pprint.pformat(cats) + '\n'))
+fileObj.close()
